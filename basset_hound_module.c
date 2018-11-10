8b7b19a0ab181480b92ad6b973bbb48462f9dc7c
Rayne C <rdc4042>
2018-11-10 15:14:53 -0500

    Fixed non-ascii characters

--- basset_hound_module.c
+++ basset_hound_module.c
@@ -9,20 +9,20 @@ Description: Kernel module to export the kernel task list.
 #include <linux/kernel.h>
 
 static int __init basset_hound_init(void) {
-	printk(KERN_INFO “Hello, World!\n”);
+	printk(KERN_INFO "Hello, World!\n");
 	return 0;
 }
 
 static void __exit basset_hound_exit(void) {
-	printk(KERN_INFO “Goodbye, World!\n”);
+	printk(KERN_INFO "Goodbye, World!\n");
 }
 
 
 module_init(basset_hound_init);
 module_exit(basset_hound_exit);
 
-MODULE_LICENSE(“GPL”);
-MODULE_AUTHOR(“Rayne Cafaro & Jonathan Jang”);
-MODULE_DESCRIPTION(“A Linux kernel module to export the kernel task list.”);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rayne Cafaro & Jonathan Jang");
+MODULE_DESCRIPTION("A Linux kernel module to export the kernel task list.");
 
 
