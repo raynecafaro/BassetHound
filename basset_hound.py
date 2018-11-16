e95162419c6f587a3c41f6e7cc06ec9b310b9441
Rayne Cafaro <rdc4042@rit.edu>
2018-11-16 06:28:54 -0500

    Fully functional rootkit detection

--- basset_hound.py
+++ basset_hound.py
@@ -1,18 +1,19 @@
 # Rayne Cafaro & Jonathan Jang
 from subprocess import Popen, PIPE, STDOUT
-import argparse, os
+import argparse, os, sys
 
 class BassetHound(object):
-    def __init__(self, lkm, use_ignore_list) -> None:
+    def __init__(self, lkm) -> None:
+        if os.getuid():
+            print("basset_hound.py must be run with UID 0 [root or sudo].")
+            sys.exit(1)
+
         if lkm is None:
             self.lkm = "./basset_hound_module.ko"
         else:
             self.lkm = lkm
 
-        if use_ignore_list is None:
-            self.use_ignore_list = 1
-        else:
-            self.use_ignore_list = 0
+        self.compare()
         
     def parse_ps(self) -> tuple:
         cmd = "ps -ef | awk \'{print $2}\'"
@@ -21,7 +22,7 @@ class BassetHound(object):
         process_id_list = list()
         for line in output:
             process_id_list.append(int(line.decode()))
-        
+       
         process_id_list_count = len(process_id_list)
         
         return (process_id_list, process_id_list_count)
@@ -29,19 +30,29 @@ class BassetHound(object):
     def parse_ko_out(self) -> tuple:
         lines = list()
         
-        # TODO: try block for priv level
-        # os.system("dmesg --clear")
+        cmd = "rmmod basset_hound_module.ko"
+        Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        
+        cmd = "dmesg --clear"
+        Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+
+        cmd = "insmod basset_hound_module.ko"
+        Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+
         cmd = 'dmesg'
         p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
         output = p.stdout.readlines()
         
-        # TODO: Throw Kernel Module not loaded error if no "==" present
         for line in output:
             if "==" in line.decode():
                 lines.append(line.decode())
+        if len(lines) is 0:
+            print("The kernel module does not appear to have loaded.")
+            sys.exit(1)
+
 
         task_list = lines[:-1]
-        task_list_count = int(lines[-1].split()[-1].strip('[').strip(']'))
+        task_list_count = int(lines[-1].split()[-1].strip('[]'))
 
         if len(task_list) is not task_list_count:
             print("ALERT: The dmesg task list count does not match exported task list.")
@@ -53,44 +64,45 @@ class BassetHound(object):
         task_list, task_list_count = self.parse_ko_out()
 
         if process_id_list_count is not task_list_count:
-            print("ALERT: The process list count does not match the task list count.\n")
             for task in task_list:
-                stripped_task = int(task.split()[-1].strip('[').strip(']'))
+                stripped_task = int(task.split()[-1].strip('[]'))
                 if stripped_task not in process_id_list:
-                    if self.use_ignore_list:
-                        if "insmod" not in task and "systemd-udev" not in task:
-                            print(task)
+                    if "insmod" in task:
+                        process_id_list_count = process_id_list_count - 1
+                        print("Probably not a hidden process: " + task)
+                        continue
 
-            print("Task List Count: " + str(task_list_count))
-            print("Process ID List Count: " + str(process_id_list_count))
-        
+                    if "systemd-udev" in task:
+                        process_id_list_count = process_id_list_count - 1
+                        print("Probably not a hidden process: " + task)
+                        continue
+
+                    if "kworker/" in task:
+                        process_id_list_count = process_id_list_count - 1
+                        print("Probably not a hidden process: " + task)
+                        continue
+                    
+                    print("Hidden process: " + task)
+                    
+            
+            if process_id_list_count is not task_list_count:
+                print("ALERT: Hidden process indicates a potential rootkit.\n")
+            else:
+                print("No hidden processes detected.\n")
+
+            print("Kernel Task List Count: " + str(task_list_count))
+            print("Userland Process List Count: " + str(process_id_list_count))
+            print()
+
         else:
             print("No hidden processes detected.")
-
-    """
-    def compare(self):
-        process_id_list, process_id_list_count = self.parse_ps()
-        task_list, task_list_count = self.parse_ko_out()
-
-        if process_id_list_count is not task_list_count:
-            print("ALERT: The process list count does not match the task list count.\n")
-            for task in task_list:
-                print(task)
-
-        else:
-            print("No hidden processes detected.")
-    """
 
 def main():
     parser = argparse.ArgumentParser(description='Detect processes hidden by a rootkit.')
     parser.add_argument('--lkm', type=str, help='Location of basset_hound_module.ko', default=None)
-    parser.add_argument('--noignore', type=bool, help='Do not ignore kernel generated processes. WARNING: Will claim to detect hidden processes', default=None)
-
-
     args = parser.parse_args()
 
-    HoundObj = BassetHound(args.lkm, args.noignore)
-    HoundObj.compare()
+    hound = BassetHound(args.lkm)
 
 if __name__ == "__main__":
     main()
